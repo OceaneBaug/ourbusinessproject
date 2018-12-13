2c5db10c24c43912a16a58a99f7e6538cdd407b5
Oceane Bauguil <oceane.bauguil@iut-rodez.fr>
2018-12-13 11:47:00 +0100

    Fix#5 question 2.1.3

--- src/main/java/ourbusinessproject/Project.java
+++ src/main/java/ourbusinessproject/Project.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -24,6 +25,11 @@ public class Project {
 	@Column(name = "description")
 	private String description;
 	
+
+	@Column(name = "enterprise")
+	@NotNull
+	private Enterprise enterprise;
+	
 	public Long getId() {return this.id ; }
 	
 	public String getTitle() {
@@ -40,6 +46,11 @@ public class Project {
 	
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	public void setEnterprise(Enterprise enterprise) {
+		this.enterprise = enterprise;
+		
 	}
 	
 	
